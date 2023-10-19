9f386e49b46c0e419b1d6cc2ed6e94eac14e6f87
chiragpatel222 <chirag.gscb@gmail.com>
2023-10-19 15:06:05 +1300

    Login Page

---
+++ src/components/Login.tsx
@@ -0,0 +1,55 @@
+function Login() {
+  return (
+    <section className="hero is-primary is-fullheight">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-5-tablet is-4-desktop is-3-widescreen">
+              <form action="" className="box">
+                <div className="field">
+                  <label className="label">Email</label>
+                  <div className="control has-icons-left">
+                    <input
+                      type="email"
+                      placeholder="e.g. chirag.patel@gmail.com"
+                      className="input"
+                      required
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fas fa-envelope"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Password</label>
+                  <div className="control has-icons-left">
+                    <input
+                      type="password"
+                      placeholder="*******"
+                      className="input"
+                      required
+                    />
+                    <span className="icon is-small is-left">
+                      <i className="fa fa-lock"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="checkbox">
+                    <input type="checkbox" />
+                    Remember me
+                  </label>
+                </div>
+                <div className="field">
+                  <button className="button is-success">Login</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Login;
